967d759286efa7ba4d51c3b663c174f22d4c2bb6
jkl0898 <jkl0898@users.noreply.github.com>
2026-03-15 12:04:37 +0800

    fix: 修复音乐播放问题并添加全站播放支持
    - 更换为免费无版权音乐源（Pixabay）
    - 为所有页面添加播放器支持
    - 修复部分歌曲无法播放的问题

--- js/music-player.js
+++ js/music-player.js
@@ -26,39 +26,40 @@
       listMaxHeight: '400px',
       
       // ⭐⭐⭐ 在这里配置你的音乐列表 ⭐⭐⭐
+      // 推荐使用免费无版权音乐：https://pixabay.com/music/
       audio: [
         {
-          name: 'Stay Hungry Stay Foolish',
-          artist: '博客主题曲',
-          url: 'https://music.163.com/song/media/outer/url?id=186016.mp3',
+          name: 'Relaxing Piano',
+          artist: 'Lesfm',
+          url: 'https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=relaxing-145038.mp3',
           cover: 'https://picsum.photos/300/300?random=1',
           lrc: ''
         },
         {
-          name: '清晨的阳光',
-          artist: '轻音乐',
-          url: 'https://music.163.com/song/media/outer/url?id=33956318.mp3',
+          name: 'Acoustic Breeze',
+          artist: 'Benjamin Tissot',
+          url: 'https://cdn.pixabay.com/download/audio/2022/03/10/audio_c8c8a73467.mp3?filename=acoustic-breeze-1505.mp3',
           cover: 'https://picsum.photos/300/300?random=2',
           lrc: ''
         },
         {
-          name: '夜的钢琴曲五',
-          artist: '石进',
-          url: 'https://music.163.com/song/media/outer/url?id=186016.mp3',
+          name: 'A Quiet Thought',
+          artist: 'Wayne Pitt',
+          url: 'https://cdn.pixabay.com/download/audio/2022/10/25/audio_946bc3eb81.mp3?filename=a-quiet-thought-by-kevin-macleod.mp3',
           cover: 'https://picsum.photos/300/300?random=3',
           lrc: ''
         },
         {
-          name: 'Rainy Day',
-          artist: 'Daydream',
-          url: 'https://music.163.com/song/media/outer/url?id=524582962.mp3',
+          name: 'Ambient Piano',
+          artist: 'Pumpupthemind',
+          url: 'https://cdn.pixabay.com/download/audio/2022/10/18/audio_69f2d2c332.mp3?filename=ambient-piano-sketches-1506.mp3',
           cover: 'https://picsum.photos/300/300?random=4',
           lrc: ''
         },
         {
-          name: '春の訪問',
-          artist: '立花慎之介',
-          url: 'https://music.163.com/song/media/outer/url?id=28446467.mp3',
+          name: 'Good Night',
+          artist: 'FASSounds',
+          url: 'https://cdn.pixabay.com/download/audio/2022/10/25/audio_946bc3eb81.mp3?filename=good-night-160166.mp3',
           cover: 'https://picsum.photos/300/300?random=5',
           lrc: ''
         }
